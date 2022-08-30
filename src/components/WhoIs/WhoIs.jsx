bfb2f8f7974b90eefbc73588fe493d35ff034478
JoyKevin1 <joykevinrobin1@gmail.com>
2022-08-30 23:26:13 +0200

    Finally done with responsiveness, What a tuff work plus restarting browse due to github errors

--- src/components/WhoIs/WhoIs.jsx
+++ src/components/WhoIs/WhoIs.jsx
@@ -1,4 +1,6 @@
 import Stranger from "../../images/stranger.png";
+import './Who.css'
+
 
 const WhoIs = () => {
   return (
@@ -7,12 +9,12 @@ const WhoIs = () => {
         <div className="h-[15%] w-[20%] flex items-center justify-center ">
           <h1 className="text-4xl text-[#e50914] font-bold">BANJOFLIX</h1>
         </div>
-        <div className=" h-[60%] flex flex-col justify-between">
+        <div className="h-[60%] flex flex-col justify-between">
           <h1 className="text-[2.5rem] text-center text-[#fff]">
             Who's watching?
           </h1>
-          <div className=" h-[70%]  flex items-start justify-center">
-            <div className="h-[67%] w-[6.5%] flex flex-col justify-between rounded-[3px] cursor-pointer">
+          <div className="h-[70%]  flex items-start justify-center">
+            <div className="h-[67%] w-[6.5%] flex flex-col justify-between rounded-[3px] cursor-pointer who-img-holder">
               <img
                 src={Stranger}
                 alt=""
