6f9b0ff1d60d57849ff9bd0bc9b6039588321802
JoyKevin1 <joykevinrobin1@gmail.com>
2022-09-07 16:19:03 +0200

    Slight change in /browse

--- src/components/Movies/RandomMov.jsx
+++ src/components/Movies/RandomMov.jsx
@@ -1,6 +1,9 @@
 import { FaInfoCircle, FaPlay } from "react-icons/fa";
+import { useRecoilState } from "recoil";
+import { movieSelected } from "../../atoms";
 
 const RandomMov = () => {
+  const [hasPlayed, setHasPlayed] = useRecoilState(movieSelected);
   return (
     <div className="h-[35%] w-1/2 flex flex-col items-start justify-around  mt-[4%] ml-[3%] randesc-holder">
       <h1 className="w-[70%] text-[3.2rem] text-[white] font-[500] movie-title">
@@ -11,7 +14,10 @@ const RandomMov = () => {
         home before they began fighting for their country in the 1920s.
       </h2>
       <div className="h-[30%] w-[35%] flex items-center justify-around movbtns-holder">
-        <button className="h-[2.8rem] w-[40%] flex items-center justify-center rounded-[4px] bg-[#e5e7eb]">
+        <button
+          className="h-[2.8rem] w-[40%] flex items-center justify-center rounded-[4px] bg-[#e5e7eb]"
+          onClick={() => setHasPlayed(true)}
+        >
           <span>
             <FaPlay
               style={{
@@ -22,7 +28,10 @@ const RandomMov = () => {
           </span>
           <h3 className="ml-[0.6rem]">Play</h3>
         </button>
-        <button className="h-[2.8rem] w-[51%] flex items-center justify-center rounded-[4px] bg-[#737373]">
+        <button
+          className="h-[2.8rem] w-[51%] flex items-center justify-center rounded-[4px] bg-[#737373]"
+          onClick={() => setHasPlayed(true)}
+        >
           <span>
             <FaInfoCircle
               style={{
