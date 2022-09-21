dfd4af67d932bcdcb89bf691607d139d2e288517
JoyKevin1 <joykevinrobin1@gmail.com>
2022-09-21 17:07:50 +0200

    Final Push this day

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import App from "./App";
 
+// Testing
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
