cf0a4b7160602f8e8dc2f9f6f802b5982b602342
JoyKevin1 <joykevinrobin1@gmail.com>
2022-09-22 22:12:08 +0200

    Nothing

--- src/utils/Protect.jsx
+++ src/utils/Protect.jsx
@@ -3,21 +3,21 @@ import { Outlet, Navigate } from "react-router-dom";
 
 const Protect = () => {
   const [so, setSo] = useState(true)
-  const [directCheck, setDirectCheck] = useState("");
-  useEffect(() => {
-    checkLogStatus();
-    directTheUser();
-  }, []);
+  // const [directCheck, setDirectCheck] = useState("");
+  // useEffect(() => {
+  //   checkLogStatus();
+  //   directTheUser();
+  // }, []);
 
-  function checkLogStatus() {
-    const logged = window.localStorage.getItem("login");
-    window.localStorage.setItem("userRedirect", logged);
-  }
+  // function checkLogStatus() {
+  //   const logged = window.localStorage.getItem("login");
+  //   window.localStorage.setItem("userRedirect", logged);
+  // }
 
-  function directTheUser() {
-    const data = window.localStorage.getItem("userRedirect");
-    setDirectCheck(true);
-  }
+  // function directTheUser() {
+  //   const data = window.localStorage.getItem("userRedirect");
+  //   setDirectCheck(true);
+  // }
 
   return so === true ? <Outlet /> : <Navigate to="/login" />;
 };
