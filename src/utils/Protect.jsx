b811ed80aeb30d5e0c66cc8af80111d8032a256d
JoyKevin1 <joykevinrobin1@gmail.com>
2022-09-23 08:34:53 +0200

    This is lit!

--- src/utils/Protect.jsx
+++ src/utils/Protect.jsx
@@ -1,15 +1,18 @@
-import { useState, useEffect } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
-function checkLogStatus() {
-  const logged = window.localStorage.getItem("login");
-  window.localStorage.setItem("userRedirect", logged);
-}
-
-checkLogStatus();
-
 const Protect = () => {
-  return window.localStorage.getItem("userRedirect") == 'Allowed' ? <Outlet /> : <Navigate to="/login" />;
+  function checkLogStatus() {
+    const logged = window.localStorage.getItem("login");
+    window.localStorage.setItem("userRedirect", logged);
+  }
+
+  checkLogStatus();
+
+  return window.localStorage.getItem("userRedirect") == "Allowed" ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" />
+  );
 };
 
 export default Protect;
